1f452359951b5ccdf283b756ca622d88927c95f2
Vishal <vg539616@gmail.com>
2024-05-14 01:16:36 +0530

    fixed no logging

--- src/main/java/org/advancedjava/Main.java
+++ src/main/java/org/advancedjava/Main.java
@@ -1,10 +1,14 @@
 package org.advancedjava;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        ObjectMapper obj = new ObjectMapper();
+//        ObjectMapper obj = new ObjectMapper();
+        logger.error("Logger initialized successfully.");
     }
 }
